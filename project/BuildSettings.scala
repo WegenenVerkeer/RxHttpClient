cabe9d9751c9455f814d1f1393a0218573208342
Karel Maesen <karel@geovise.com>
2016-04-01 15:39:17 +0200

    Version bump

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -11,7 +11,7 @@ trait BuildSettings {
   import Dependencies._
   val Organization = "be.wegenenverkeer"
   
-  val Version = "0.3.0"
+  val Version = "0.4.0-SNAPSHOT"
   val ScalaVersion = "2.11.7"
   val ScalaBuildOptions = Seq("-unchecked", "-deprecation", "-feature",
     "-language:reflectiveCalls",
