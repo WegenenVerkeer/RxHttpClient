bf0ad1e3cbb2bc4f0fe6bff4ac1f09d19790da81
Karel Maesen <karel@geovise.com>
2020-04-20 15:12:25 +0200

    Cleanup test

--- modules/fs2/src/test/scala/Fs2InteropSpecification.scala
+++ modules/fs2/src/test/scala/Fs2InteropSpecification.scala
@@ -1,6 +1,5 @@
 import be.wegenenverkeer.rxhttp.fs2.Implicits._
 import cats.effect.{ContextShift, IO}
-import org.specs2.Specification
 
 /**
  * Created by Karel Maesen, Geovise BVBA on 20/04/2020.
@@ -15,15 +14,8 @@ class Fs2InteropSpecification extends org.specs2.mutable.Specification
     "provide an a FS2-compliant streaming interface" in {
       val expectBody = "{ 'contacts': [1,2,3] }"
 
-      import com.github.tomakehurst.wiremock.client.{WireMock => wm}
-      server.stubFor(wm.get(wm.urlPathEqualTo("/contacts"))
-        .withQueryParam("q", wm.equalTo("test"))
-        .withHeader("Accept", wm.equalTo("application/json"))
-        .willReturn(wm.aResponse.withStatus(200)
-          .withHeader("Content-Type", "application/json")
-          .withBody(expectBody)
-        )
-      )
+
+      stub(expectBody)
 
       import scala.concurrent.ExecutionContext
       implicit val contextShift: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
@@ -38,21 +30,11 @@ class Fs2InteropSpecification extends org.specs2.mutable.Specification
 
     }
 
-    "provide a Cats-Effect Async compliant interface" in {
+    "provide a Cats-Effect Aƒsync compliant interface" in {
 
       val expectBody = "{ 'contacts': [1,2,3] }"
 
-      import com.github.tomakehurst.wiremock.client.{WireMock => wm}
-      server.stubFor(wm.get(wm.urlPathEqualTo("/contacts"))
-        .withQueryParam("q", wm.equalTo("test"))
-        .withHeader("Accept", wm.equalTo("application/json"))
-        .willReturn(wm.aResponse.withStatus(200)
-          .withHeader("Content-Type", "application/json")
-          .withBody(expectBody)
-        )
-      )
-
-
+      stub(expectBody)
       //set up use case
 
       import scala.concurrent.ExecutionContext
@@ -67,4 +49,18 @@ class Fs2InteropSpecification extends org.specs2.mutable.Specification
       output must_== expectBody
     }
   }
+
+  def stub(expectBody: String) = {
+    import com.github.tomakehurst.wiremock.client.{WireMock => wm}
+    server.stubFor(wm.get(wm.urlPathEqualTo("/contacts"))
+      .withQueryParam("q", wm.equalTo("test"))
+      .withHeader("Accept", wm.equalTo("application/json"))
+      .willReturn(wm.aResponse.withStatus(200)
+        .withHeader("Content-Type", "application/json")
+        .withBody(expectBody)
+      )
+    )
+  }
+
+
 }
